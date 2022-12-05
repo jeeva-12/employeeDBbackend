aaba37c314d17ccf417ee701db5d15b03b256a9b
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-05 15:30:14 +0530

    first

---
+++ src/main/java/com/example/employeedb_backend/controller/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.example.employeedb_backend.controller;
+
+public class EmployeeController {
+}
---
+++ src/test/java/com/example/employeedb_backend/EmployeedbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeedb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeedbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
